b6ff9dd3efda7e60be6935767ae53374eaba1f52
OvalRaptor <114360124+OvalRaptor@users.noreply.github.com>
2024-05-06 17:32:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,16 @@
 #Install Code Server
 echo -e "\033[32m Installing code server on Termux \033[0m"
 apt-get -y update
-apt-get -y install nodejs-lts
-apt-get -y install tur-repo 
-apt-get -y install code-server
+apt-get -y install tur-repo build-essential gdb python python2 nodejs openjdk-17 php ruby golang rust git yarn ripgrep git mariadb postgresql
+apt-get -y install mongodb code-server
+gem install bundler
+
+#Configure Databases
+mysql_install_db
+mysqld_safe -u root &
+initdb ~/../usr/var/lib/postgresql
+pg_ctl -D ~/../usr/var/lib/postgresql start
+mongod --dbpath ~/../usr/var/lib/mongodb --logpath ~/../usr/var/log/mongodb/mongod.log --fork
 
 #Install Bashrc
 cd /data/data/com.termux/files/usr/etc && cp -v bash.bashrc $HOME/VSCodeOnAndroid/backup/ && cd -
